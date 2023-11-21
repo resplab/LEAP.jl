c3127f06de817eba48b7e36f2e6cf6844d629c1e
Ainsleigh <ainsleighhill@gmail.com>
2023-11-20 16:26:47 -0800

    Add docstrings for asthma_utility

--- src/modules/asthma_utility.jl
+++ src/modules/asthma_utility.jl
@@ -1,15 +1,43 @@
+"""
+    Utility
+
+A struct for the satisfaction, or utility, that a person has given their asthma status.
+
+For example, having asthma is worse than not having asthma, so this would have a lower utility value.
+Likewise, more asthma exacerbations is worse than less asthma exacerbations, so this would
+also have a lower utility value.
+
+# Fields
+- `parameters::Union{AbstractDict, Nothing}`: A dictionary containing the following keys:
+    `exac`, `control`, `eq5d`.
+
+"""
 struct Utility <: Utility_Module
-    parameters
+    parameters::Union{AbstractDict, Nothing}
 end
 
-function process(ag::Agent,util::Utility)
-    # baseline util
-    baseline = util.parameters[:eq5d][(ag.age,Int(ag.sex))].eq5d[1]
-    if !ag.has_asthma
-        # no asthma, so return the baseline value
+
+"""
+    process(agent, utility)
+
+Compute the economic utility due to asthma.
+
+If the person (agent) does not have asthma, the utility is the baseline utility.
+If the person (agent) does have asthma, compute the reduced utility due to asthma exacerbations and
+asthma control.
+
+# Arguments
+- `agent::Agent`: An agent, see [`Agent`](@ref).
+- `utility::Utility`: A utility object, see [`Utility`](@ref).
+
+"""
+function process(agent::Agent, utility::Utility)
+    baseline = utility.parameters[:eq5d][(agent.age, Int(agent.sex))].eq5d[1]
+    if !agent.has_asthma
         return baseline
     else
-        # disutility due to having exacerbation and control
-        return max(0,baseline - sum(ag.exac_sev_hist[1] .* util.parameters[:exac]) - sum(ag.control .* util.parameters[:control]))
+        util_exacerbation = sum(agent.exac_sev_hist[1] .* utility.parameters[:exac])
+        util_control = sum(agent.control .* utility.parameters[:control])
+        return max(0, (baseline - util_exacerbation - util_control))
     end
 end
