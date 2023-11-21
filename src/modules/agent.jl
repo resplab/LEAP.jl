05914ee982ca4d1bf9ccc6bf66b104e92b5c7172
Ainsleigh <ainsleighhill@gmail.com>
2023-11-20 16:03:08 -0800

    add docstrings for set_agent!

--- src/modules/agent.jl
+++ src/modules/agent.jl
@@ -39,25 +39,62 @@ struct Agent  <: Agent_Module
     asthma_status::Bool
 end
 
-function set_agent!(ag, sex, age,cal_year,cal_year_index,alive, num_antibiotic_use,
-    has_asthma,asthma_age,asthma_severity,asthma_control,asthma_exac_hist,asthma_exac_sev_history,total_hosp,fam_hist,asthma_status)
-    ag.sex = sex
-    ag.age= age
-    ag.cal_year = cal_year
-    ag.cal_year_index = cal_year_index
-    ag.alive = alive
-    ag.num_antibiotic_use=num_antibiotic_use
-    ag.has_asthma = has_asthma
-    ag.asthma_age = asthma_age
-    ag.severity = asthma_severity
-    ag.control = asthma_control
-    ag.exac_hist = asthma_exac_hist
-    ag.exac_sev_hist = asthma_exac_sev_history
-    ag.total_hosp = total_hosp
-    ag.family_hist = fam_hist
-    ag.asthma_status = asthma_status
+
+"""
+    set_agent!(agent, sex, age, cal_year, cal_year_index, alive, num_antibiotic_use,
+        has_asthma, asthma_age, asthma_severity, asthma_control, asthma_exac_hist,
+        asthma_exac_sev_history, total_hosp, fam_hist, asthma_status)
+
+Given an agent object and parameters, initialize the values of the agent object.
+
+# Arguments
+- `agent::Agent`: An agent, see [`Agent`](@ref).
+- `sex::Bool`: Sex of person, true = male, false = female.
+- `age::Integer`: Age of person in years.
+- `cal_year::Integer`: The calendar year, e.g. 2020.
+- `cal_year_index::Integer`: An integer representing the year of the simulation. For example, if the
+    simulation starts in 2023, then the `cal_year_index` for 2023 is 1, for 2024 is 2, etc.
+- `alive::Bool`: Whether the person is alive, true = alive.
+- `num_antibiotic_use::Integer`: TODO.
+- `has_asthma::Bool`: Whether the person has astham, true = has asthma.
+- `asthma_age::Union{Nothing, Integer}`: Age at which the person was diagnosed with asthma.
+- `asthma_severity::Union{Nothing, Integer}`: Asthma severity level: 1 = mild, 2 = severe.
+- `asthma_control::Union{Nothing, Vector{Integer}}`: Asthma control level: 1 = uncontrolled,
+    2 = partially controlled, 3 = fully controlled.
+- `asthma_exac_hist::Union{Nothing, Vector{Integer}}`: Total number of asthma exacerbations.
+- `asthma_exac_sev_hist::Union{Nothing, Vector{Vector{Integer}}}`: Number of asthma exacerbations by
+    severity.
+- `total_hosp::Integer`: Total number of very severe asthma exacerbations leading to
+    hospitalization.
+- `fam_hist::Bool`: Is there a family history of asthma?
+- `asthma_status::Bool`: TODO.
+
+"""
+function set_agent!(agent::Agent, sex::Bool, age::Integer, cal_year::Integer,
+    cal_year_index::Integer, alive::Bool, num_antibiotic_use::Integer,
+    has_asthma::Bool, asthma_age::Union{Nothing, Integer}, asthma_severity::Union{Nothing, Integer},
+    asthma_control::Union{Nothing, Vector{Integer}},
+    asthma_exac_hist::Union{Nothing, Vector{Integer}},
+    asthma_exac_sev_history::Union{Nothing, Vector{Vector{Integer}}}, total_hosp::Integer,
+    fam_hist::Bool, asthma_status::Bool)
+    agent.sex = sex
+    agent.age= age
+    agent.cal_year = cal_year
+    agent.cal_year_index = cal_year_index
+    agent.alive = alive
+    agent.num_antibiotic_use = num_antibiotic_use
+    agent.has_asthma = has_asthma
+    agent.asthma_age = asthma_age
+    agent.severity = asthma_severity
+    agent.control = asthma_control
+    agent.exac_hist = asthma_exac_hist
+    agent.exac_sev_hist = asthma_exac_sev_history
+    agent.total_hosp = total_hosp
+    agent.family_hist = fam_hist
+    agent.asthma_status = asthma_status
     nothing
 end
+
 
 function process_initial(ag::Agent,asthma_age_data)
     if ag.age==0
