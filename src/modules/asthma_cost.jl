26f9941762e50ab24888c052f2924f7c0b7b2bb3
Ainsleigh <ainsleighhill@gmail.com>
2023-11-20 16:15:58 -0800

    add docstrings for Cost module

--- src/modules/asthma_cost.jl
+++ src/modules/asthma_cost.jl
@@ -1,13 +1,39 @@
+"""
+    Cost
+
+A struct for the financial cost of asthma.
+
+# Fields
+- `parameters::Union{AbstractDict, Nothing}`: A dictionary containing the following keys:
+    `exac`, `control`.
+
+"""
 struct Cost <: Cost_Module
-    parameters
+    parameters::Union{AbstractDict, Nothing}
 end
 
-function process(ag::Agent,cost::Cost)
-    if !ag.has_asthma
-        # no asthma, so return 0
+
+"""
+    process(agent, cost)
+
+Compute the financial cost due to asthma.
+
+If the person (agent) does not have asthma, the cost is $0.
+If the person (agent) does have asthma, compute the cost due to asthma exacerbations
+and asthma control.
+
+# Arguments
+- `agent::Agent`: An agent, see [`Agent`](@ref).
+- `cost::Cost`: A cost object, see [`Cost`](@ref).
+
+"""
+function process(agent::Agent, cost::Cost)
+    if !agent.has_asthma
         return 0
     else
-        # costs due to having exacerbation and control
-        return sum(ag.exac_sev_hist[1] .* cost.parameters[:exac]) + sum(ag.control .* cost.parameters[:control])
+        return (
+            sum(agent.exac_sev_hist[1] .* cost.parameters[:exac]) +
+            sum(agent.control .* cost.parameters[:control]
+        )
     end
 end
