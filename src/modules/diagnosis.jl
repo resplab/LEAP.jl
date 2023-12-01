4f653718bdf3b00bdcad42f70a550fe5c5bf70bb
Ainsleigh <ainsleighhill@gmail.com>
2023-12-01 13:36:13 -0800

    fix new line bug in diagnosis

--- src/modules/diagnosis.jl
+++ src/modules/diagnosis.jl
@@ -9,11 +9,9 @@ function agent_has_asthma(agent::Agent, diagnosis::Diagnosis)
         agent.has_asthma
     else
         if agent.has_asthma # if yes asthma, can get mis dx of not having asthma
-            rand(Bernoulli(diagnosis.table[min(max_year, agent.cal_year_index)]
-            [agent.age - 3, agent.sex + 3]))
+            rand(Bernoulli(diagnosis.table[min(max_year, agent.cal_year_index)][agent.age - 3, agent.sex + 3]))
         else # if no asthma but then can get mis dx of asthma
-            rand(Bernoulli(diagnosis.table_mis[min(agent.cal_year_index, max_year)]
-            [agent.age-3, agent.sex+3]))
+            rand(Bernoulli(diagnosis.table_mis[min(agent.cal_year_index, max_year)][agent.age-3, agent.sex+3]))
         end
     end
 end
