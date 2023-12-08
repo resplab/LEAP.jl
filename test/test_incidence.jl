2ecf2b9a1831f87de241bf04545151bb4cf5b177
Ainsleigh <ainsleighhill@gmail.com>
2023-12-08 15:00:52 -0800

    update test_incidence

--- test/test_incidence.jl
+++ test/test_incidence.jl
@@ -3,14 +3,23 @@ using LEAP
 using DataFrames
 
 @testset "test agent_has_asthma" begin
-    simulation = LEAP.set_up(111, "CA", 2001, 40, 10, "M3");
+    max_age = 111
+    province = "CA"
+    starting_year = 2001
+    time_horizon = 40
+    n = 10
+    population_growth_type = "M3"
+    incidence = LEAP.set_up_incidence(starting_year, province)
+    birth = LEAP.set_up_birth(starting_year, population_growth_type, province)
+    antibiotic_exposure = LEAP.set_up_antibiotic_exposure()
+    family_history = LEAP.set_up_family_history()
     cal_year = 2002
     tmp_cal_year_index = cal_year - 2001 + 1
     agent = process_birth(
-        cal_year, tmp_cal_year_index, simulation.birth,
-        simulation.antibioticExposure, simulation.familyHistory
+        cal_year, tmp_cal_year_index, birth,
+        antibiotic_exposure, family_history
     )
     @test agent.has_asthma == false
-    agent_has_asthma(agent, simulation.incidence)
+    agent_has_asthma(agent, incidence)
 
 end
