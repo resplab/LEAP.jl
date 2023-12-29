6aaf06aa64fa9b482fd5789804a22f437a9657bc
Ainsleigh <ainsleighhill@gmail.com>
2023-12-29 12:52:38 -0800

    add census_division to Agent arguments

--- src/modules/agent.jl
+++ src/modules/agent.jl
@@ -23,6 +23,7 @@ A person in the model.
 - `total_hosp::Integer`: Total number of very severe asthma exacerbations leading to hospitalization.
 - `family_hist::Bool`: Is there a family history of asthma?
 - `asthma_status::Bool`: TODO.
+- `census_division::CensusDivisionModule`: the Canadian census division where the agent resides.
 """
 @kwdef struct Agent  <: Agent_Module
     sex::Bool
@@ -40,32 +41,34 @@ A person in the model.
     total_hosp::Integer
     family_hist::Bool
     asthma_status::Bool
+    census_division::CensusDivisionModule
 end
 
-function set_agent!(ag, sex, age,cal_year,cal_year_index,alive, num_antibiotic_use,
-    has_asthma,asthma_age,asthma_severity,asthma_control,asthma_exac_hist,asthma_exac_sev_history,total_hosp,fam_hist,asthma_status)
-    ag.sex = sex
-    ag.age= age
-    ag.cal_year = cal_year
-    ag.cal_year_index = cal_year_index
-    ag.alive = alive
-    ag.num_antibiotic_use=num_antibiotic_use
-    ag.has_asthma = has_asthma
-    ag.asthma_age = asthma_age
-    ag.severity = asthma_severity
-    ag.control = asthma_control
-    ag.exac_hist = asthma_exac_hist
-    ag.exac_sev_hist = asthma_exac_sev_history
-    ag.total_hosp = total_hosp
-    ag.family_hist = fam_hist
-    ag.asthma_status = asthma_status
-    nothing
+function set_agent!(agent, sex, age,cal_year,cal_year_index,alive, num_antibiotic_use,
+    has_asthma,asthma_age,asthma_severity,asthma_control,asthma_exac_hist,asthma_exac_sev_history,
+    total_hosp,fam_hist, asthma_status, census_division)
+    agent.sex = sex
+    agent.age= age
+    agent.cal_year = cal_year
+    agent.cal_year_index = cal_year_index
+    agent.alive = alive
+    agent.num_antibiotic_use=num_antibiotic_use
+    agent.has_asthma = has_asthma
+    agent.asthma_age = asthma_age
+    agent.severity = asthma_severity
+    agent.control = asthma_control
+    agent.exac_hist = asthma_exac_hist
+    agent.exac_sev_hist = asthma_exac_sev_history
+    agent.total_hosp = total_hosp
+    agent.family_hist = fam_hist
+    agent.asthma_status = asthma_status
+    agent.census_division = census_division
 end
 
-function process_initial(ag::Agent,asthma_age_data)
-    if ag.age==0
+function process_initial(agent::Agent,asthma_age_data)
+    if agent.age==0
         return 0
     else
-        return StatsBase.sample(Weights(asthma_age_data[1:ag.age+1,Int(ag.sex)+1]))-1
+        return StatsBase.sample(Weights(asthma_age_data[1:agent.age+1,Int(agent.sex)+1]))-1
     end
 end
