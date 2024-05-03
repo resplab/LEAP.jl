f2f60f879bdfa58bef19770e02613300637f2d85
Ainsleigh <ainsleighhill@gmail.com>
2024-05-03 13:38:42 -0700

    add test for compute_prob_emigration

--- test/test_emigration.jl
+++ test/test_emigration.jl
@@ -16,6 +16,38 @@ function test_emigration()
     end
 end
 
+
+function test_compute_prob_emigration()
+    @testset "test compute_prob_emigration age 0" begin
+        province = "BC"
+        age = 0
+        sex = false
+        starting_year = 2020
+        cal_year_index = 4
+        population_growth_type = "FA"
+        emigration = LEAP.Emigration(starting_year, province, population_growth_type)
+        @test LEAP.compute_prob_emigration(cal_year_index, age, sex, emigration) == 0
+    end
+    @testset "test compute_prob_emigration non-zero" begin
+        province = "BC"
+        age = 99
+        sex = true
+        starting_year = 2020
+        cal_year_index = 4
+        population_growth_type = "M2"
+        emigration = LEAP.Emigration(starting_year, province, population_growth_type)
+        true_count = 0
+        for i in 1:1000000
+            if LEAP.compute_prob_emigration(cal_year_index, age, sex, emigration)
+                true_count += 1
+            end
+        end
+        @test round(true_count, sigdigits=3) <= 436000
+        @test round(true_count, sigdigits=3) >= 433000
+    end
+end
+
 @testset "test emigration" begin
     test_emigration()
+    test_compute_prob_emigration()
 end
