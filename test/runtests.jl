8ebb75aa47cb467e5117972b852333c706ef4b4e
Ainsleigh <ainsleighhill@gmail.com>
2024-05-06 12:39:12 -0700

    add ARGS to runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,24 +4,67 @@ using LEAP
 
 @testset "All Tests" begin
 
-include("test_agent.jl")
-include("test_antibioticexposure.jl")
-include("test_asthmacost.jl")
-include("test_birth.jl")
-include("test_censusdivision.jl")
-include("test_control.jl")
-include("test_death.jl")
-include("test_emigration.jl")
-include("test_exacerbation.jl")
-include("test_familyhistory.jl")
-include("test_immigration.jl")
-include("test_incidence.jl")
-include("test_outcomematrix.jl")
-include("test_pollution.jl")
-include("test_prevalence.jl")
-include("test_reassessment.jl")
-include("test_severity.jl")
-include("test_simulation.jl")
-include("test_utility.jl")
+    if isempty(ARGS) ||  "all" in ARGS
+        all_tests = true
+    else
+        all_tests = false
+    end
 
+    if all_tests || "agent" in ARGS
+        include("test_agent.jl")
+    end
+    if all_tests || "antibiotic" in ARGS
+        include("test_antibioticexposure.jl")
+    end
+    if all_tests || "cost" in ARGS
+        include("test_asthmacost.jl")
+    end
+    if all_tests || "birth" in ARGS
+        include("test_birth.jl")
+    end
+    if all_tests || "census" in ARGS
+        include("test_censusdivision.jl")
+    end
+    if all_tests || "control" in ARGS
+        include("test_control.jl")
+    end
+    if all_tests || "death" in ARGS
+        include("test_death.jl")
+    end
+    if all_tests || "emigration" in ARGS
+        include("test_emigration.jl")
+    end
+    if all_tests || "exacerbation" in ARGS
+        include("test_exacerbation.jl")
+    end
+    if all_tests || "family_history" in ARGS
+        include("test_familyhistory.jl")
+    end
+    if all_tests || "immigration" in ARGS
+        include("test_immigration.jl")
+    end
+    if all_tests || "incidence" in ARGS
+        include("test_incidence.jl")
+    end
+    if all_tests || "outcome_matrix" in ARGS
+        include("test_outcomematrix.jl")
+    end
+    if all_tests || "pollution" in ARGS
+        include("test_pollution.jl")
+    end
+    if all_tests || "prevalence" in ARGS
+        include("test_prevalence.jl")
+    end
+    if all_tests || "reassessment" in ARGS
+        include("test_reassessment.jl")
+    end
+    if all_tests || "severity" in ARGS
+        include("test_severity.jl")
+    end
+    if all_tests || "simulation" in ARGS
+        include("test_simulation.jl")
+    end
+    if all_tests || "utility" in ARGS
+        include("test_utility.jl")
+    end
 end
