eed421a9a186762eb949db17096794b39e73dc42
Ainsleigh <ainsleighhill@gmail.com>
2024-05-06 12:43:28 -0700

    fix outcome_matrix tests

--- test/test_outcomematrix.jl
+++ test/test_outcomematrix.jl
@@ -18,8 +18,10 @@ using DataFrames, JSON
     min_cal_year = simulation.min_cal_year
     max_cal_year = simulation.max_cal_year
     cal_years = min_cal_year:max_cal_year
-    outcome_matrix = LEAP.create_outcome_matrix(until_all_die, cal_years, min_cal_year,
-       max_cal_year, max_age)
+    outcome_matrix = LEAP.create_outcome_matrix(
+        until_all_die=until_all_die, cal_years=cal_years, min_cal_year=min_cal_year,
+        max_cal_year=max_cal_year, max_age=max_age
+    )
 
     @test size(outcome_matrix.control) == (40, 112, 2, 3)
     @test size(outcome_matrix.exacerbation_by_severity) == (40, 112, 2, 4)
@@ -43,8 +45,10 @@ end
     min_cal_year = simulation.min_cal_year
     max_cal_year = simulation.max_cal_year
     cal_years = min_cal_year:max_cal_year
-    outcome_matrix = LEAP.create_outcome_matrix(until_all_die, cal_years, min_cal_year,
-       max_cal_year, max_age)
+    outcome_matrix = LEAP.create_outcome_matrix(
+        until_all_die=until_all_die, cal_years=cal_years, min_cal_year=min_cal_year,
+        max_cal_year=max_cal_year, max_age=max_age
+    )
 
     LEAP.increment_field_in_outcome_matrix!(outcome_matrix, "antibiotic_exposure",
         2, false, 1, 3)
