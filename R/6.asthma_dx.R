e963aa9b4fd815292dad3371b26d6fa9502f8c51
Ainsleigh <ainsleighhill@gmail.com>
2025-05-25 16:57:48 -0700

    6 - cleanup indentation and variable names

--- R/6.asthma_dx.R
+++ R/6.asthma_dx.R
@@ -36,84 +36,70 @@ get_reassessment_data <- function(
     
   }
   
-  xs <- expand.grid(age=3:110,sex=c(0,1),year=starting_year:end_year) %>% 
-    as.data.frame()
+    df_asthma <- expand.grid(age=3:110, sex=c(0,1), year=starting_year:end_year) %>% 
+        as.data.frame()
   
-  df_asthma <- xs %>% 
-    mutate(inc = asthma_predictor(age,sex,year,type='inc'),
-           prev = asthma_predictor(age,sex,year,type='prev')) %>% 
-    # let inc be prev for age = 3
-    mutate(inc = ifelse(age==3,prev,inc))
+    df_asthma <- df_asthma %>% 
+        mutate(
+            inc=asthma_predictor(age,sex,year,type='inc'),
+            prev=asthma_predictor(age,sex,year,type='prev')
+        ) %>% 
+        # let inc be prev for age = 3
+        mutate(inc=ifelse(age==3, prev, inc))
   
-  df_asthma_year <- df_asthma %>% 
-    group_split(year)
+    df_asthma_year <- df_asthma %>% group_split(year)
   
-  results_assessment <- c()
-  years <- c((starting_year+1):end_year)
-  ages <- c(4:110)
+    results_assessment <- c()
+    years <- c((starting_year+1):end_year)
+    ages <- c(4:110)
   
-  for( i in 1:(length(years)-1)){
+    for( i in 1:(length(years)-1)){
     
-    tmp_year <- years[i]
+        tmp_year <- years[i]
     
-    tmp_prev_past <- df_asthma_year[[i]] %>% 
-      select(age,year,sex,prev) %>% 
-      filter(age!=110) %>%
-      pivot_wider(names_from=sex,values_from=prev) %>% 
-      select(-age,-year)
+        # Get the predicted prevalence for the previous year
+        df_prevalence_past <- df_asthma_year[[i]] %>% 
+            select(age, year, sex, prev) %>% 
+            filter(age!=110) %>%
+            pivot_wider(names_from=sex, values_from=prev) %>% 
+            select(-age, -year)
     
-    tmp_prev <- df_asthma_year[[i+1]] %>% 
-      select(age,year,sex,prev) %>% 
-      filter(age!=3) %>% 
-      pivot_wider(names_from=sex,values_from=prev)  %>% 
-      select(-age,-year)
+        # Get the predicted prevalence for the current year
+        df_prevalence <- df_asthma_year[[i+1]] %>% 
+            select(age, year, sex, prev) %>% 
+            filter(age!=3) %>% 
+            pivot_wider(names_from=sex, values_from=prev)  %>% 
+            select(-age,-year)
     
-    tmp_inc <- df_asthma_year[[i+1]] %>% 
-      select(age,year,sex,inc) %>% 
-      filter(age!=3) %>% 
-      pivot_wider(names_from=sex,values_from=inc)  %>% 
-      select(-age,-year)
-    
-    # prev = prev_past*p(reassessment) + inc*(1-prev_past)
-    # p(reassessment) = (prev-inc*(1-prev_past))/prev_past
+        # Get the predicted incidence for the current year
+        df_incidence <- df_asthma_year[[i+1]] %>% 
+            select(age, year, sex, inc) %>% 
+            filter(age!=3) %>% 
+            pivot_wider(names_from=sex, values_from=inc)  %>% 
+            select(-age, -year)
 
-    # estimate tmp_u by assuming p(correct diagnosis) = 1 
-    tmp_assessment <- (tmp_prev - tmp_inc * (1-tmp_prev_past))/tmp_prev_past
-    
-    # # key step: bound it!
-    # tmp_u[tmp_u>1] <- 1
-    # tmp_u[tmp_u<0] <- 0
+        # estimate tmp_u by assuming P(correct diagnosis) = 1 
+        df_assessment <- (df_prevalence - df_incidence * (1 - df_prevalence_past)) / df_prevalence_past
   
-    results_assessment[[i]] <- cbind(data.frame(age=4:110,year=tmp_year),tmp_assessment)
-  }
+        results_assessment[[i]] <- cbind(
+            data.frame(age=4:110, year=year),
+            df_assessment
+        )
+    }
   
-  results_assessment <- results_assessment %>%
-    do.call(rbind,.) %>% 
-    as.data.frame()
+    df_reassessment <- results_assessment %>%
+        do.call(rbind, .) %>% 
+        as.data.frame()
   
-  colnames(results_assessment)[c(3,4)] <- c("F","M")
-  
-  results_assessment$province <- chosen_province
-  
-  return(results_assessment)
+    colnames(df_reassessment)[c(3, 4)] <- c("F", "M")
+    df_reassessment$province <- chosen_province
+
+    return(df_reassessment)
 }
 
 
 CA_tuner <- get_reassessment_data(chosen_province="CA",end_year=2066,stabilization_year = 2025)
 BC_tuner <- get_reassessment_data(chosen_province="BC",end_year=2043,stabilization_year=2025)
-
-# look <- results_assessment %>%
-#   do.call(rbind,.) %>%
-#   pivot_longer(3:4,names_to="sex",values_to="reassessment") %>%
-#   as.data.frame()
-# 
-# tmp <- gam(log(reassessment)~sex + s(year,age),data=look)
-# 
-# ggplot(data = look %>% filter(age==12),
-#        aes(y=reassessment,x=year)) +
-#   geom_line()+
-#   ylim(0,1.1)+
-#   facet_grid(.~sex)
 
 master_assessment <- rbind(CA_tuner,
                            BC_tuner)
