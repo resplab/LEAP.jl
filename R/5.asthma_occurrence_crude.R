038eb6c4a857676d88e62c14ae70fe72716e602c
Ainsleigh <ainsleighhill@gmail.com>
2025-05-26 15:18:55 -0700

    5 - factor out plot_occurrence

--- R/5.asthma_occurrence_crude.R
+++ R/5.asthma_occurrence_crude.R
@@ -286,45 +286,47 @@ write_csv(df, here("R/master_asthma_prev_inc.csv"))
 df <- df %>% 
     mutate(sex=ifelse(sex==1, "Male", "Female")) 
 
-year_min <- STARTING_YEAR
-year_max <- STABILIZATION_YEAR
-year_step <- 5
-years <- seq(year_min, year_max, by=year_step)
-title <- "Crude Asthma Prevalence (per 100)"
-ggplot(
-    data=df %>% 
-        filter(year %in% years) %>% 
-        mutate(year=as.factor(year)),
-    aes(x=age, y=prev, col=year)
-) +
-    geom_line() +
-    xlim(c(0, 60)) +
-    facet_grid(.~sex) +
-    ylab(title)+
-    xlab("Age (years)")+
-    theme_classic(base_size=20) +
-    theme(
-        legend.position='top',
-        legend.title=element_blank()
-    )
+plot_occurrence <- function(
+    df, title, y_col, year_min=2000, year_max=2020, year_step=2
+) {
+    years <- seq(year_min, year_max, by=year_step)
+    ggplot(
+        data=df %>% 
+            filter(year %in% years) %>% 
+            mutate(year=as.factor(year)),
+        aes(x=age, y=!!rlang::sym(y_col), color=year)
+    ) +
+        geom_line() +
+        xlim(c(0, 60)) +
+        facet_grid(.~sex) +
+        ylab(title) +
+        xlab("Age (years)")+
+        theme_classic(base_size=20) +
+        theme(
+            legend.position='top',
+            legend.title=element_blank()
+        )
+}
 
-title <- "Crude Asthma Incidence (per 100)"
-ggplot(
-    data=df %>% 
-        filter(year %in% years) %>% 
-        mutate(year=as.factor(year)),
-    aes(x=age, y=inc, col=year)
-) +
-    geom_line() +
-    xlim(c(0, 60)) +
-    facet_grid(.~sex) +
-    ylab(title)+
-    xlab("Age (years)")+
-    theme_classic(base_size=20) +
-    theme(
-        legend.position='top',
-        legend.title=element_blank()
-    )
+ 
+plot_occurrence(
+    df=df,
+    title="Crude Asthma Prevalence (per 100)",
+    y_col="prev",
+    year_min=STARTING_YEAR,
+    year_max=STABILIZATION_YEAR,
+    year_step=5
+)
+
+
+plot_occurrence(
+    df=df,
+    title="Crude Asthma Incidence (per 100)",
+    y_col="inc",
+    year_min=STARTING_YEAR,
+    year_max=STABILIZATION_YEAR,
+    year_step=5
+)
 
 write_csv(df, here("R/master_asthma_prev_inc.csv"))
 
